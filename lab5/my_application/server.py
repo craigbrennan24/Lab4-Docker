bccd374636af5f69e32c05907690c36ecaf08cae
craigbrennan24 <craig_brennan24@hotmail.com>
2015-10-21 14:59:41 +0000

    Modifying server.py

--- lab5/my_application/server.py
+++ lab5/my_application/server.py
@@ -2,8 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+    return "Index page\n"
+
+@app.route('/hello')
 def hello():
-    return "Hello World!"
+    return "Hello world!\n"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8080,debug=True)
